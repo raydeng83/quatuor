44376a3f032f14d832cfb9203d1425e90b557c7e
Le Deng <lede@Les-MacBook-Pro.local>
2016-08-12 16:45:50 -0400

    latest

---
+++ frontend/app/components/registration.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'registration',
+  templateUrl: 'app/components/registration.component.html'
+})
+export class RegistrationComponent {
+
+}
